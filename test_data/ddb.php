66e2d3a197c33973f438ff95614372c33e4f51de
SonnyBoyWilliamsom <SonnyBoyWilliamsom@users.noreply.github.com>
2015-08-18 09:47:12 +0200

    Update ddb.php
    Data

--- test_data/ddb.php
+++ test_data/ddb.php
@@ -1,8 +1,101 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php
+$cursos = array(
+            array('id_curso'=>'1', 'nombre'=>'MDI','profesor'=>'Jairo'),
+            array('id_curso'=>'2', 'nombre'=>'MDA','profesor'=>'Jairo'),
+            array('id_curso'=>'3', 'nombre'=>'SPP','profesor'=>'Jairo'),
+            array('id_curso'=>'4', 'nombre'=>'VFX','profesor'=>'Jairo'),
+            array('id_curso'=>'5', 'nombre'=>'MOSM','profesor'=>'Jairo'),
+            array('id_curso'=>'6', 'nombre'=>'JAVA','profesor'=>'Jairo'),
+            array('id_curso'=>'7', 'nombre'=>'DJ','profesor'=>'Jairo'),
+            array('id_curso'=>'8', 'nombre'=>'IOS','profesor'=>'Jairo')
+        );
+$unidades = array(
+            array('id_unidad'=>'1', 'id_curso'=>'1','nombre'=>'U1-a'),
+            array('id_unidad'=>'2', 'id_curso'=>'1','nombre'=>'U2-d'),
+            array('id_unidad'=>'3', 'id_curso'=>'1','nombre'=>'U3-r'),
+            array('id_unidad'=>'4', 'id_curso'=>'1','nombre'=>'U4-g'),
+            array('id_unidad'=>'5', 'id_curso'=>'1','nombre'=>'U5-q'),
+            array('id_unidad'=>'6', 'id_curso'=>'1','nombre'=>'U6-c'),
+            array('id_unidad'=>'7', 'id_curso'=>'1','nombre'=>'U7-k'),
+            array('id_unidad'=>'8', 'id_curso'=>'1','nombre'=>'U8-i'),
+            array('id_unidad'=>'9', 'id_curso'=>'1','nombre'=>'U9-p'),
+            array('id_unidad'=>'10', 'id_curso'=>'1','nombre'=>'U10-ñ'),
+            array('id_unidad'=>'11', 'id_curso'=>'2','nombre'=>'U1-V'),
+            array('id_unidad'=>'12', 'id_curso'=>'2','nombre'=>'U2-GH'),
+            array('id_unidad'=>'13', 'id_curso'=>'2','nombre'=>'U3-FF'),
+        );
+$preguntas = array( 
+                array('id_pregunta'=>'12', 'id_unidad'=>'1','numero'=>'1','pregunta'=>'Suponiendo que esta sea la pregunta uno de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'23', 'id_unidad'=>'1','numero'=>'2','pregunta'=>'Suponiendo que esta sea la pregunta 2 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'36', 'id_unidad'=>'1','numero'=>'3','pregunta'=>'Suponiendo que esta sea la pregunta 3 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'44', 'id_unidad'=>'1','numero'=>'4','pregunta'=>'Suponiendo que esta sea la pregunta 4 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'45', 'id_unidad'=>'1','numero'=>'5','pregunta'=>'Suponiendo que esta sea la pregunta 5 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'46', 'id_unidad'=>'1','numero'=>'6','pregunta'=>'Suponiendo que esta sea la pregunta 6 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'48', 'id_unidad'=>'1','numero'=>'7','pregunta'=>'Suponiendo que esta sea la pregunta 7 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'34', 'id_unidad'=>'1','numero'=>'8','pregunta'=>'Suponiendo que esta sea la pregunta 8 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'11', 'id_unidad'=>'1','numero'=>'9','pregunta'=>'Suponiendo que esta sea la pregunta 9 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'45', 'id_unidad'=>'1','numero'=>'10','pregunta'=>'Suponiendo que esta sea la pregunta 10 de una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                array('id_pregunta'=>'50', 'id_unidad'=>'1','numero'=>'11','pregunta'=>'Pregunta uno de la unidad uno del curso 2 -MDA de mentira'),
+                array('id_pregunta'=>'51', 'id_unidad'=>'11','numero'=>'2','pregunta'=>'pregunta dos de la unidad uno del curso 2 - MDA'),
+                array('id_pregunta'=>'52', 'id_unidad'=>'11','numero'=>'3','pregunta'=>'pregunta tres de la unidad uno del curso 2 - MDA'),
+                array('id_pregunta'=>'53', 'id_unidad'=>'11','numero'=>'4','pregunta'=>'pregunta cuatro de la unidad uno del curso 2 - MDA'),
+                array('id_pregunta'=>'54', 'id_unidad'=>'11','numero'=>'5','pregunta'=>'pregunta cinco de la unidad uno del curso 2 - MDA'),
+                array('id_pregunta'=>'55', 'id_unidad'=>'11','numero'=>'6','pregunta'=>'pregunta seis de la unidad uno del curso 2 - MDA'),
+                array('id_pregunta'=>'56', 'id_unidad'=>'11','numero'=>'7','pregunta'=>'pregunta siete de la unidad uno del curso 2 - MDA'),
+    
+            );
+$respuestas = array(
+                            array('id_respuesta'=>'1', 'id_pregunta'=>'12', 'opcion'=>'a', 'descripcion'=>'Suponiendo que una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'2', 'id_pregunta'=>'12', 'opcion'=>'b','descripcion'=>' Que esta 999sea la pregunta uno de una unidad que pertenece a un curso, definir como se relacionaría la misma con su cuiente'),
+                            array('id_respuesta'=>'3', 'id_pregunta'=>'23', 'opcion'=>'a','descripcion'=>'Supde 6668 unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'4', 'id_pregunta'=>'23', 'opcion'=>'b','descripcion'=>'Unidad 66correspondiente'),
+                            array('id_respuesta'=>'5', 'id_pregunta'=>'44', 'opcion'=>'a','descripcion'=>'La mis555ma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'7', 'id_pregunta'=>'44', 'opcion'=>'b', 'descripcion'=>'Su666666poniendo que una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'9', 'id_pregunta'=>'36', 'opcion'=>'a','descripcion'=>' Que esta sea la pregunta uno de una unidad que pertenece a un curso, definir como se relacionaría la misma con su cuiente'),
+                            array('id_respuesta'=>'10', 'id_pregunta'=>'36', 'opcion'=>'b','descripcion'=>'Supde una u444444444nidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'11', 'id_pregunta'=>'45', 'opcion'=>'a','descripcion'=>'Unidad c222orrespondiente'),
+                            array('id_respuesta'=>'12', 'id_pregunta'=>'46', 'opcion'=>'a','descripcion'=>'La misma333333 con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'13', 'id_pregunta'=>'45', 'opcion'=>'b', 'descripcion'=>'Suponiendo que555555 una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'14', 'id_pregunta'=>'46', 'opcion'=>'b','descripcion'=>' Que e22222222sta sea la pregunta uno de una unidad que pertenece a un curso, definir como se relacionaría la misma con su cuiente'),
+                            array('id_respuesta'=>'15', 'id_pregunta'=>'48', 'opcion'=>'a','descripcion'=>'Supde una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'16', 'id_pregunta'=>'48', 'opcion'=>'b','descripcion'=>'Unidad co4544444444rrespondiente'),
+                            array('id_respuesta'=>'17', 'id_pregunta'=>'34', 'opcion'=>'a','descripcion'=>'La misma 5con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'18', 'id_pregunta'=>'34', 'opcion'=>'b', 'descripcion'=>'Suponiendo 345345345345que una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'19', 'id_pregunta'=>'11', 'opcion'=>'a','descripcion'=>' Que esta 4564564sea la pregunta uno de una unidad que pertenece a un curso, definir como se relacionaría la misma con su cuiente'),
+                            array('id_respuesta'=>'20', 'id_pregunta'=>'11', 'opcion'=>'b','descripcion'=>'Supde una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta'=>'21', 'id_pregunta'=>'50', 'opcion'=>'c','descripcion'=>'Supde 50 '),
+                            array('id_respuesta'=>'22', 'id_pregunta'=>'50', 'opcion'=>'a','descripcion'=>'Supde 50!'),
+                            array('id_respuesta'=>'23', 'id_pregunta'=>'51', 'opcion'=>'b','descripcion'=>'Supde 51'),
+                            array('id_respuesta'=>'24', 'id_pregunta'=>'51', 'opcion'=>'a','descripcion'=>'Supde 51!'),
+                            array('id_respuesta'=>'25', 'id_pregunta'=>'52', 'opcion'=>'b','descripcion'=>'Supde 52'),
+                            array('id_respuesta'=>'26', 'id_pregunta'=>'52', 'opcion'=>'c','descripcion'=>'Supde 52!'),
+                            array('id_respuesta'=>'27', 'id_pregunta'=>'53', 'opcion'=>'b','descripcion'=>'Supde 53'),
+                            array('id_respuesta'=>'28', 'id_pregunta'=>'53', 'opcion'=>'a','descripcion'=>'Supde 53!'),
+                            array('id_respuesta'=>'29', 'id_pregunta'=>'54', 'opcion'=>'b','descripcion'=>'Supde una 54'),
+                            array('id_respuesta'=>'30', 'id_pregunta'=>'54', 'opcion'=>'d','descripcion'=>'Supde 54'),
+                            array('id_respuesta'=>'31', 'id_pregunta'=>'55', 'opcion'=>'c','descripcion'=>'Supde una 55'),
+                            array('id_respuesta'=>'32', 'id_pregunta'=>'55', 'opcion'=>'b','descripcion'=>'Supde 55'),
+                            array('id_respuesta'=>'33', 'id_pregunta'=>'56', 'opcion'=>'c','descripcion'=>'Supde una 56'),
+                            array('id_respuesta'=>'34', 'id_pregunta'=>'56', 'opcion'=>'a','descripcion'=>'Supde 56')
+                            
+                        );
+$respuestas_correctas = array(
+                            array('id_respuesta_true'=>'1', 'id_pregunta'=>'12', 'opcion'=>'a', 'descripcion'=>'Suponiendo que una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta_true'=>'2', 'id_pregunta'=>'23', 'opcion'=>'b','descripcion'=>' Que esta sea la pregunta uno de una unidad que pertenece a un curso, definir como se relacionaría la misma con su cuiente'),
+                            array('id_respuesta_true'=>'3', 'id_pregunta'=>'44', 'opcion'=>'b','descripcion'=>'Supde una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta_true'=>'4', 'id_pregunta'=>'36', 'opcion'=>'a','descripcion'=>'Unidad correspondiente'),
+                            array('id_respuesta_true'=>'4', 'id_pregunta'=>'45', 'opcion'=>'a','descripcion'=>'Unidad dfdfdfdfcorrespondiente'),
+                            array('id_respuesta_true'=>'5', 'id_pregunta'=>'46', 'opcion'=>'d','descripcion'=>'La misma con su curso y unidad correspondiente'),
+                            array('id_respuesta_true'=>'1', 'id_pregunta'=>'48', 'opcion'=>'a', 'descripcion'=>'Suponiendo que una unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta_true'=>'2', 'id_pregunta'=>'34', 'opcion'=>'b','descripcion'=>' Que esta sea la pregunta uno de una unidad que pertenece a un curso, definir como se relacionaría la misma con su cuiente'),
+                            array('id_respuesta_true'=>'3', 'id_pregunta'=>'11', 'opcion'=>'b','descripcion'=>'Supde unsdfdfgdfga unidad que pertenece a un curso, definir como se relacionaría la misma con su curso y unidad correspondiente'),
+                            array('id_respuesta_true'=>'4', 'id_pregunta'=>'45', 'opcion'=>'a','descripcion'=>'Unidad cfgdfgdfgdfgdfgorrespondiente')
+                         
+                        );
+$alumnos = array(
+                array('id'=>'1', 'id_curso'=>'1', 'nombre'=>'Jaimito Borromeo'),
+                array('id'=>'2', 'id_curso'=>'6', 'nombre'=>'Eusebio Matalascañas'),
+                array('id'=>'3', 'id_curso'=>'8', 'nombre'=>'Gonzalo  Rubalcaba'),
+                array('id'=>'4', 'id_curso'=>'3', 'nombre'=>'Maite Hontele')
+);
